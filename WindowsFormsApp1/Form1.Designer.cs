938720086bc85c9c0fb589776bb8e6acba920d04
Wean <438864121@qq.com>
2017-04-17 21:33:18 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtAllMsg = new System.Windows.Forms.TextBox();
+            this.cmbIP = new System.Windows.Forms.ComboBox();
+            this.txtSendMsg = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "添加好友IP";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "查找好友IP";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(325, 284);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(325, 30);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "添加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(113, 32);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(206, 21);
+            this.txtIP.TabIndex = 4;
+            // 
+            // txtAllMsg
+            // 
+            this.txtAllMsg.Location = new System.Drawing.Point(113, 107);
+            this.txtAllMsg.Multiline = true;
+            this.txtAllMsg.Name = "txtAllMsg";
+            this.txtAllMsg.Size = new System.Drawing.Size(206, 109);
+            this.txtAllMsg.TabIndex = 5;
+            // 
+            // cmbIP
+            // 
+            this.cmbIP.FormattingEnabled = true;
+            this.cmbIP.Location = new System.Drawing.Point(113, 61);
+            this.cmbIP.Name = "cmbIP";
+            this.cmbIP.Size = new System.Drawing.Size(206, 20);
+            this.cmbIP.TabIndex = 6;
+            // 
+            // txtSendMsg
+            // 
+            this.txtSendMsg.Location = new System.Drawing.Point(113, 237);
+            this.txtSendMsg.Multiline = true;
+            this.txtSendMsg.Name = "txtSendMsg";
+            this.txtSendMsg.Size = new System.Drawing.Size(206, 70);
+            this.txtSendMsg.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "所有消息";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "发送消息";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 388);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSendMsg);
+            this.Controls.Add(this.cmbIP);
+            this.Controls.Add(this.txtAllMsg);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "WeeChater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtAllMsg;
+        private System.Windows.Forms.ComboBox cmbIP;
+        private System.Windows.Forms.TextBox txtSendMsg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private UdpClient udpReceive;
+        private UdpClient udpSend;
+        private Thread thread;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            udpReceive = new UdpClient(8888);
+            thread = new Thread(ReceiveMessage);
+            thread.IsBackground = true;
+            thread.Start();
+
+        }
+
+
+        private void ReceiveMessage()
+        {
+            IPEndPoint remoteIEP = new IPEndPoint(IPAddress.Any,0);
+            while (true)
+            {
+                if (udpReceive.Available == 0)
+                {
+                    continue;
+                }
+                byte[] buffer = udpReceive.Receive(ref remoteIEP);
+                string words = Encoding.UTF8.GetString(buffer);
+                showMsg(remoteIEP.ToString() + ":" + words);
+            }
+        }
+
+        private void showMsg(string a)
+        {
+
+            txtAllMsg.AppendText(a + "\t\n");
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showMsg("自己说："+txtSendMsg.Text);
+            udpSend = new UdpClient();
+            IPEndPoint iep = new IPEndPoint(IPAddress.Parse(cmbIP.Text),8888);
+            //把要传输的字符串转化成字节流，用容器盛放
+            byte[] buffer = Encoding.UTF8.GetBytes(txtSendMsg.Text);
+            int retry = 0;
+            while (true)
+            {
+                try
+                {
+                    udpSend.Send(buffer, buffer.Length, iep);
+                    break;
+                }
+                catch
+                {
+                    if (retry < 3)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        showMsg(txtSendMsg.Text + "发送失败");
+                        break;
+                      
+                    }
+                }
+            }
+            txtSendMsg.Text = "";
+            txtSendMsg.Focus();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            
+            cmbIP.Items.Add(txtIP.Text);
+            MessageBox.Show(txtIP.Text + "成功添加");
+           
+        }
+    }
+}
